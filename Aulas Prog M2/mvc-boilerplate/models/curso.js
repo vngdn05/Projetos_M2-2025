d9f03e1c660f63fb3bfcacab9e47f2447e2c5038
Vinicius Lucas Ciardi <vinicius.ciardi@sou.inteli.edu.br>
2025-05-20 11:56:02 -0300

    aula 20/05/2025

--- Aulas Prog M2/mvc-boilerplate/models/curso.js
+++ Aulas Prog M2/mvc-boilerplate/models/curso.js
@@ -6,22 +6,19 @@ module.exports = {
     return result.rows;
   },
 
-  // Função no model para atualizar o nome de um curso pelo ID
-async update(id, nome) {
-  const query = 'UPDATE curso SET nome = $1 WHERE id = $2 RETURNING *';
-  const result = await db.query(query, [nome, id]);
-  return result.rows[0];
-},
-
   async create(nome) {
     const query = 'INSERT INTO curso (nome) VALUES ($1) RETURNING *';
     const result = await db.query(query, [nome]);
     return result.rows[0];
   },
 
-  // Função no model que remove um curso do banco de dados pelo ID
-async delete(id) {
-  await db.query('DELETE FROM curso WHERE id = $1', [id]);
-},
-};
+  async update(id, nome) {
+    const query = 'UPDATE curso SET nome = $1 WHERE id = $2 RETURNING *';
+    const result = await db.query(query, [nome, id]);
+    return result.rows[0];
+  },
 
+  async delete(id) {
+    await db.query('DELETE FROM curso WHERE id = $1', [id]);
+  }
+};
